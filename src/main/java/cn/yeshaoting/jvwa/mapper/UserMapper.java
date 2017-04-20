3ed837c4f582227d8c3c4481dc1281b8f6dd201d
叶绍亭 <yeshaoting@meituan.com>
2017-04-20 16:16:38 +0800

    add sql

--- src/main/java/cn/yeshaoting/jvwa/mapper/UserMapper.java
+++ src/main/java/cn/yeshaoting/jvwa/mapper/UserMapper.java
@@ -18,16 +18,16 @@ public interface UserMapper {
 
     @Insert("insert into user(username, stage, create_time, update_time) values(#{user.username}, #{user.stage}, unix_timestamp(), unix_timestamp())")
     @ResultType(java.lang.Integer.class)
-    @Options(useCache = false, useGeneratedKeys = true, keyProperty = "user.id", keyColumn = "id")
+    @Options(useCache = true, useGeneratedKeys = true, keyProperty = "user.id", keyColumn = "id")
     int insert(@Param("user") User user);
     
     @Insert("update user set stage = #{user.stage}, update_time = unix_timestamp() where username = #{user.username}")
     @ResultType(java.lang.Integer.class)
-    @Options(useCache = false)
+    @Options(useCache = true)
     int update(@Param("user") User user);
 
-    @Select("select id, username, stage from user where username = #{username}")
+    @Select("select id, username, stage, create_time createTime, update_time updateTime from user where username = #{username}")
     @ResultType(User.class)
-    @Options(useCache = false)
+    @Options(useCache = true)
     User findUserByUsername(@Param("username") String username);
 }
